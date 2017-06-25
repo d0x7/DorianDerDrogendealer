66602bdef6c52097da8973477300f52f26b33786
tosch901 <tosch960@gmail.com>
2017-06-25 15:55:15 +0200

    Removed unnecessary return

--- src/sample/Deal.java
+++ src/sample/Deal.java
@@ -2,10 +2,10 @@ package sample;
 
 
 public class Deal extends DorianDerDrogendealer {
+
     public static void main(String[] args) {
 
         DorianDerDrogendealer dealer = new DorianDerDrogendealer();
         dealer.deal();
-        return;
     }
 }
